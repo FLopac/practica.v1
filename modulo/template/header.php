c854ff44000f4e5d0bf9974cbd1348224c01db4e
FLopac-note <flopezc99@gmail.com>
2022-04-28 11:06:37 -0400

    Se modificó el header, titulo de la ventana

--- modulo/template/header.php
+++ modulo/template/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="es">
   <head>
-    <title class="text-light bg-dark" >Crud php y mysql b5</title>
+    <title class="text-light bg-dark" >RemuneWeb</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
